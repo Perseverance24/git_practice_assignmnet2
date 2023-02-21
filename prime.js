7c2eaf8e18166401dd569ec5e83b793714ed0a02
Rahul Sharma <rahusharma.sharma@gmail.com>
2023-02-21 16:43:10 +0530

    prime added

---
+++ prime.js
@@ -0,0 +1,18 @@
+let num = 10;
+let count = 0;
+
+for(let i=1; i<=num; i++)
+{
+  if(num % i == 0)
+  {
+    count;
+  }
+}
+
+if(count == 2)
+{
+  console.log("Prime");
+}
+ else{
+  console.log("Not Prime"); 
+}
